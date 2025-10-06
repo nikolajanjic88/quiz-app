65a1532861c87ee0fd58fa54acf129d36831ff31
Nikola Janjic <nikola.janjic88rm@gmail.com>
2025-10-07 00:58:59 +0200

    fix guess char

--- public/js/guess.js
+++ public/js/guess.js
@@ -1,24 +1,24 @@
-
 const characterImageElement = document.getElementById('characterImage');
-let userGuess = document.getElementById('guess-input').value.trim();
 const apiUrl = '/silmarilion-quiz-app-lore/get'; 
 let lore = []
 let currentCharacter = {};
 
-function play() {
+function play() 
+{
     document.getElementById('guess-input').value = ''; // ✅ Clear input
     document.getElementById('result').innerText = ''; // Optional: clear result
     result.className = '';
 
-    if (lore.length === 0) {
-    fetch(apiUrl)
-        .then((res) => res.json())
-        .then((data) => {
-        lore = data;
-        getRandomCharacter(lore);
+    if (lore.length === 0) 
+    {
+        fetch(apiUrl)
+            .then((res) => res.json())
+            .then((data) => {
+            lore = data;
+            getRandomCharacter(lore);
         });
     } else {
-    getRandomCharacter(lore);
+        getRandomCharacter(lore);
     }
 }
 
@@ -29,22 +29,34 @@ function getRandomCharacter(chars)
     characterImageElement.src =currentCharacter.image;     
 }
 
-function checkGuess() {
-    const userGuess = document.getElementById('guess-input').value.trim(); // ✅ Get updated input
+function clear()
+{
+    document.getElementById('guess-input').addEventListener('focus', () => {
+        result.textContent = '';
+    });
 
-    if (!userGuess) {
-    result.textContent = 'Please enter a guess.';
-    result.className = 'wrong';
-    return;
+}
+
+function checkGuess() 
+{
+    const userGuess = document.getElementById('guess-input').value.trim();
+
+    if (!userGuess) 
+    {
+        result.textContent = 'Please enter a guess.';
+        result.className = 'wrong';
+        return;
     }
 
-    if (userGuess.toLowerCase() === currentCharacter.title.toLowerCase()) {
-    result.textContent = 'Correct! Well done!';
-    result.className = 'correct';
+    if (userGuess.toLowerCase() === currentCharacter.title.toLowerCase()) 
+    {
+        result.textContent = 'Correct! Well done!';
+        result.className = 'correct';
     } else {
-    result.textContent = 'Try again!';
-    result.className = 'wrong';
+        result.textContent = 'Try again!';
+        result.className = 'wrong';
     }
 }
 
 play();
+clear();
